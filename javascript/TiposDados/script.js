7629856815d2a5f31c7dcc41f254383bb4c966fb
Lucas Correia <lucasdevproject@gmail.com>
2024-11-19 10:03:22 -0300

    Tipos de Dados - string

--- javascript/TiposDados/script.js
+++ javascript/TiposDados/script.js
@@ -5,3 +5,39 @@ var time; // Undefined
 var comida = null; // Null
 var simbolo = Symbol() // Symbol
 var novoObjeto = {} // Object
+
+//ver o tipo de dados 
+console.log(typeof nome, typeof idade);
+
+//concatenação
+ var meuNome = 'Lucas';
+ var sobrenome = 'Correia';
+ var nomeCompleto = meuNome + ' ' + sobrenome;
+ console.log(nomeCompleto);
+
+ var gols = 1000;
+ var frase = 'Romario fez ' + gols + ' gols';
+ console.log(frase);
+
+
+//Exercícios
+
+// Declare uma variável contendo uma string
+var animal = 'Gato';
+// Declare uma variável contendo um número dentro de uma string
+var idosos = '566';
+// Declare uma variável com a sua idade
+var minhaIdade = 22;
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var nome2 = 'Lucas';
+var sobrenome2 = 'Correia';
+var nomeCompleto2 = nome2 + ' ' + sobrenome2;
+console.log(nomeCompleto2);
+// Coloque a seguinte frase em uma variável: It's time
+var frase2 = " It's time "
+console.log(frase2);
+
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof nome2);
