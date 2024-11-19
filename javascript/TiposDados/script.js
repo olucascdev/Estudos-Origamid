b8221050401fc49eb1af3a1c11bc20dd3af0cc02
Lucas Correia <lucasdevproject@gmail.com>
2024-11-19 09:31:43 -0300

    Variaveis e Tipos de Dados

---
+++ javascript/TiposDados/script.js
@@ -0,0 +1,7 @@
+var nome = 'André'; // String
+var idade = 28; // Number
+var possuiFaculdade = true; // Boolean
+var time; // Undefined
+var comida = null; // Null
+var simbolo = Symbol() // Symbol
+var novoObjeto = {} // Object
---
+++ javascript/variaveis/script.js
@@ -0,0 +1,40 @@
+var nome = 'Lucas';
+var idade = 22;
+var possuiFaculdade = true;
+
+console.log(nome, idade, possuiFaculdade);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+console.log(totalPreco);
+
+var sobrenome = 'Lucas', cidade = 'SM';
+
+console.log(sobrenome, cidade);
+
+var Semdefinir;
+console.log(Semdefinir);
+
+var time = 'Vasco';
+time = 'Flamengo';
+console.log(time);
+
+//Exercícios 
+
+// Declarar uma variável com o seu nome
+ var nomeCompleto = 'Lucas Correia';
+ console.log(nomeCompleto);
+// Declarar uma variável com a sua idade
+ var minhaIdade = 22;
+ console.log(minhaIdade);
+// Declarar uma variável com a sua comida
+ var comidaFav = 'Churrasco';
+ console.log(comidaFav);
+// favorita e não atribuir valor
+ var minhaComidaFav;
+// Atribuir valor a sua comida favorita
+ var minhaComidaFav = 'Churrasco 2.0'
+// Declarar 5 variáveis diferentes sem valores
+ var estado, cep, qtd , online, animal ;
+ console.log(estado,cep,qtd,online,animal);
