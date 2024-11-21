bf2cc60629c1b94fbecde6cdeee225fe44ee7cd5
Lucas Correia <lucasdevproject@gmail.com>
2024-11-21 09:29:52 -0300

    funçoes

--- javascript/Funções/script.js
+++ javascript/Funções/script.js
@@ -100,27 +100,47 @@ console.log(ehTruthy(-5));
 
 // Crie uma função matemática que retorne o perímetro de um quadrado
 // lembrando: perímetro é a soma dos quatro lados do quadrado
+function perimetro(lado){
+  return lado * 4;
+}
+console.log(perimetro(4));
 
 // Crie uma função que retorne o seu nome completo
 // ela deve possuir os parâmetros: nome e sobrenome
+function nomeCompleto(nome,sobrenome){
+  return nome + ' ' + sobrenome;
+
+}
+console.log(nomeCompleto('Lucas','Correia'));
 
 // Crie uma função que verifica se um número é par
+function ehpar(x){
+  return x % 2===0;
+}
+console.log(ehpar(2));
 
 // Crie uma função que retorne o tipo de
 // dado do argumento passado nela (typeof)
-
+function qualDado(dado){
+  return typeof dado;
+}
+console.log(qualDado('oi'));
 // addEventListener é uma função nativa do JavaScript
 // o primeiro parâmetro é o evento que ocorre e o segundo o Callback
 // utilize essa função para mostrar no console o seu nome completo
 // quando o evento 'scroll' ocorrer.
 
+addEventListener('scroll', function() {
+  console.log('Lucas Correia');
+});
+
 // Corrija o erro abaixo
+var totalPaises = 193;
 function precisoVisitar(paisesVisitados) {
-  var totalPaises = 193;
   return `Ainda faltam ${totalPaises - paisesVisitados} países para visitar`;
 }
 function jaVisitei(paisesVisitados) {
   return `Já visitei ${paisesVisitados} do total de ${totalPaises} países`;
 }
-precisoVisitar(20);
-jaVisitei(20);
+console.log(precisoVisitar(20));
+console.log(jaVisitei(20));
